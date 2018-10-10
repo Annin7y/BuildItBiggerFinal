f06c6bc5f87312d6907a55484efc41e9f3d7fa79
AnaVox6y <ana_35an@yahoo.ca>
2018-10-09 22:02:41 -0400

    update:refactor

--- jokeslibrary/src/main/java/builditbigger/android/my/annin/jokeslibrary/JokesActivity.java
+++ jokeslibrary/src/main/java/builditbigger/android/my/annin/jokeslibrary/JokesActivity.java
@@ -1,9 +1,15 @@
 package builditbigger.android.my.annin.jokeslibrary;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class JokesActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes);
 
+    }
 
 }
