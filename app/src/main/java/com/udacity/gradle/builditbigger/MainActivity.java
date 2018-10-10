32cae41ae8e1b0b4e47a09ca54c7739474cb766b
AnaVox6y <ana_35an@yahoo.ca>
2018-10-10 13:57:13 -0400

    update:refactor

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -8,18 +9,18 @@ import android.view.View;
 import android.widget.Toast;
 
 import builditbigger.android.my.annin.jokesdepository.JokesFetch;
+import builditbigger.android.my.annin.jokeslibrary.JokesActivity;
 
 
 public class MainActivity extends AppCompatActivity
 {
-      //  public static final String JOKE_STRING = "Joke string";
+    public static final String JOKE_STRING = "Joke string";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     @Override
@@ -47,17 +48,18 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-   // Intent intent = new Intent(getContext(), JokesActivity.class);
-   // intent.setAction(Intent.ACTION_SEND);
-     // intent.putExtra( MainActivity.JOKE_STRING, myJokesFetch.getRandomJoke());
-  //  startActivity(intent);
-
-
     public void tellJoke(View view)
     {
         JokesFetch myJokesFetch = new JokesFetch();
+
         //default code replaced with the code referencing JokesLibrary
-       // Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, myJokesFetch.getRandomJoke(), Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        //Toast me
+        // .2ssage commented out after adding JokesActivity class
+        // Toast.makeText(this, myJokesFetch.getRandomJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokesActivity.class);
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(JOKE_STRING, myJokesFetch.getRandomJoke());
+        startActivity(intent);
     }
 }
--- jokeslibrary/src/main/java/builditbigger/android/my/annin/jokeslibrary/JokesActivity.java
+++ jokeslibrary/src/main/java/builditbigger/android/my/annin/jokeslibrary/JokesActivity.java
@@ -1,19 +1,23 @@
 package builditbigger.android.my.annin.jokeslibrary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class JokesActivity extends AppCompatActivity {
+
+    public static final String JOKE_STRING = "Joke string";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jokes);
 
-       // Intent intent = getIntent();
-       // String joke = intent.getStringExtra(MainActivity.);
-       // TextView textView = findViewById(R.id.joke_android);
-       // textView.setText(joke);
+        Intent intent = getIntent();
+        String joke = intent.getStringExtra(JOKE_STRING);
+        TextView textView = findViewById(R.id.joke_android);
+        textView.setText(joke);
 
     }
 
