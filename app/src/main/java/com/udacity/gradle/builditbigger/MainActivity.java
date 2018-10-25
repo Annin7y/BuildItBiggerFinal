d7d9c6b4462ebb4d615c3bc79f5a682e36dc7b1a
AnaVox6y <ana_35an@yahoo.ca>
2018-10-24 22:23:55 -0400

    update code: refactor

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements EndpointsAsyncTas
     }
 
     @Override
-    public void returnJokeData() {
-        
+    public returnJokeData(result) {
+
     }
 }
