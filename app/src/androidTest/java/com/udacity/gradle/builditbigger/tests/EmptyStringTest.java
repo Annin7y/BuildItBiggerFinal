a61b91e964b29ccb2a739520f0a144442ac215c5
AnaVox6y <ana_35an@yahoo.ca>
2018-11-02 16:36:14 -0400

    update code: refactor

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/EmptyStringTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/EmptyStringTest.java
@@ -1,6 +1,41 @@
 package com.udacity.gradle.builditbigger.tests;
 
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
+import com.udacity.gradle.builditbigger.EndpointsAsyncTaskInterface;
+
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
 
 public class EmptyStringTest {
+
+//    Context context;
+//
+//    @Test
+//    public void testVerifyJoke() throws InterruptedException {
+//        assertTrue(true);
+//        final CountDownLatch latch = new CountDownLatch(1);
+//        context = InstrumentationRegistry.getContext();
+//        EndpointsAsyncTask testTask = new EndpointsAsyncTask(EndpointsAsyncTaskInterface) {
+//            @Override
+//            protected void onPostExecute(String result) {
+//                assertNotNull(result);
+//                if (result != null){
+//                    assertTrue(result.length() > 0);
+//                    latch.countDown();
+//                }
+//            }
+//        };
+//        testTask.execute(context);
+//        latch.await();
+//    }
+//
+
 }
