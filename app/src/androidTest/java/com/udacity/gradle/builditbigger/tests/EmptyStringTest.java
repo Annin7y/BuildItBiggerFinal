14bfd5472591ab7a5d73accfcf5d7e050908f7e3
AnaVox6y <ana_35an@yahoo.ca>
2018-11-07 14:34:59 -0500

    update code: refactor

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/EmptyStringTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/EmptyStringTest.java
@@ -7,6 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 import com.udacity.gradle.builditbigger.EndpointsAsyncTaskInterface;
+import com.udacity.gradle.builditbigger.MainActivity;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,48 +25,28 @@ public class EmptyStringTest {
     //https://stackoverflow.com/questions/2321829/android-asynctask-testing-with-android-test-framework/5722193#5722193
     //https://gist.github.com/he9lin/2195897
 
-    public void testAsyncTask () throws Throwable {
+    public void testAsyncTask() throws Throwable {
         // create  a signal to let us know when our task is done.
         final CountDownLatch signal = new CountDownLatch(1);
 
-        final EndpointsAsyncTask myTask = new EndpointsAsyncTask();
-
+        final EndpointsAsyncTaskInterface myTask = new EndpointsAsyncTaskInterface() {
             @Override
-            protected String doInBackground(Void... params) {
-                //Do something meaningful.
-                return "something happened!";
+            public void returnJokeData(String result) {
+
+                assertNotNull(result);
+                signal.countDown();
             }
+        };
 
 
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            /* This is the key, normally you would use some type of listener
-             * to notify your activity that the async call was finished.
-             *
-             * In your test method you would subscribe to that and signal
-             * from there instead.
-             */
-
-            signal.countDown();
-        }
-    };
-
-
-
-    ;
-// Execute the async task on the UI thread! THIS IS KEY!
         runTestOnUiThread(new Runnable() {
+            Context context = new MainActivity();
 
             @Override
             public void run() {
-                myTask.execute();
+                myTask.execute(context);
             }
         });
-
         signal.await(30, TimeUnit.SECONDS);
-        assertTrue();
-
     }
-    }
+}
