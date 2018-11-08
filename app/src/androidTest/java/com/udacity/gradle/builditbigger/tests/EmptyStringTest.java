a4452f8a3a36cb9fcc7388f8e30fa3b76c584770
AnaVox6y <ana_35an@yahoo.ca>
2018-11-08 17:03:24 -0500

    update code: refactor

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/EmptyStringTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/EmptyStringTest.java
@@ -1,13 +1,10 @@
 package com.udacity.gradle.builditbigger.tests;
 
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 import com.udacity.gradle.builditbigger.EndpointsAsyncTaskInterface;
-import com.udacity.gradle.builditbigger.MainActivity;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,20 +13,20 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static junit.framework.TestCase.assertNotNull;
-import static junit.framework.TestCase.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
-public class EmptyStringTest {
+public class EmptyStringTest  {
 
     //The code for the test is based on the following:
     //https://stackoverflow.com/questions/2321829/android-asynctask-testing-with-android-test-framework/5722193#5722193
     //https://gist.github.com/he9lin/2195897
 
+    @Test
     public void testAsyncTask() throws Throwable {
         // create  a signal to let us know when our task is done.
         final CountDownLatch signal = new CountDownLatch(1);
 
-        final EndpointsAsyncTaskInterface myTask = new EndpointsAsyncTaskInterface() {
+        final EndpointsAsyncTaskInterface myTaskInterface = new EndpointsAsyncTaskInterface() {
             @Override
             public void returnJokeData(String result) {
 
@@ -37,16 +34,8 @@ public class EmptyStringTest {
                 signal.countDown();
             }
         };
-
-
-        runTestOnUiThread(new Runnable() {
-            Context context = new MainActivity();
-
-            @Override
-            public void run() {
-                myTask.execute(context);
-            }
-        });
+        new EndpointsAsyncTask(myTaskInterface).execute();
         signal.await(30, TimeUnit.SECONDS);
     }
+
 }
