2125cbd42024815c4a50db4ec03ca8dc93cfbfc1
AnaVox6y <ana_35an@yahoo.ca>
2018-11-08 19:44:45 -0500

    update code: refactor

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/EmptyStringTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/EmptyStringTest.java
@@ -15,21 +15,24 @@ import java.util.concurrent.TimeUnit;
 import static junit.framework.TestCase.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
-public class EmptyStringTest  {
+public class EmptyStringTest
+{
 
     //The code for the test is based on the following:
     //https://stackoverflow.com/questions/2321829/android-asynctask-testing-with-android-test-framework/5722193#5722193
     //https://gist.github.com/he9lin/2195897
 
     @Test
-    public void testAsyncTask() throws Throwable {
+    public void testAsyncTask() throws Throwable
+    {
         // create  a signal to let us know when our task is done.
         final CountDownLatch signal = new CountDownLatch(1);
 
-        final EndpointsAsyncTaskInterface myTaskInterface = new EndpointsAsyncTaskInterface() {
+        final EndpointsAsyncTaskInterface myTaskInterface = new EndpointsAsyncTaskInterface()
+        {
             @Override
-            public void returnJokeData(String result) {
-
+            public void returnJokeData(String result)
+            {
                 assertNotNull(result);
                 signal.countDown();
             }
@@ -37,5 +40,4 @@ public class EmptyStringTest  {
         new EndpointsAsyncTask(myTaskInterface).execute();
         signal.await(30, TimeUnit.SECONDS);
     }
-
 }
