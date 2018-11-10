95ef9227254a9153df5666c24000bc214b9a1768
AnaVox6y <ana_35an@yahoo.ca>
2018-11-10 16:39:53 -0500

    update code: refactor

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -13,39 +13,44 @@ import android.widget.Toast;
 import builditbigger.android.my.annin.jokeslibrary.JokesActivity;
 
 
-public class MainActivity extends AppCompatActivity implements EndpointsAsyncTaskInterface {
+public class MainActivity extends AppCompatActivity implements EndpointsAsyncTaskInterface
+{
     public static final String JOKE_STRING = "joke_string";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void tellJoke(View view) {
+    public void tellJoke(View view)
+    {
         // JokesFetch myJokesFetch = new JokesFetch();
 
         // default code below replaced with the code referencing JokesLibrary
