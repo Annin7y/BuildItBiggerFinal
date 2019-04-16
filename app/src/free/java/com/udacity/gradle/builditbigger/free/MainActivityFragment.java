b2599a9b9fe60932e190870a0b31016f7cc030a4
Maino96-10022 <anast3222@gmail.com>
2019-04-15 22:22:25 -0400

    update code: interstitial ad

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
@@ -41,7 +41,7 @@ public class MainActivityFragment extends BaseMainActivityFragment
         mAdView.loadAd(adRequest);
         return root;
     }
-    
+
 @Override
     public void tellJoke(View view)
     {
