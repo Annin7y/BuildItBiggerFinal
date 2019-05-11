b0898ae64ce512ff189d7b0f191845ed2f2747bd
Maino96-10022 <anast3222@gmail.com>
2019-05-11 01:21:51 -0400

    update: interstitial ad

--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -1,15 +1,21 @@
 package com.udacity.gradle.builditbigger.paid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.udacity.gradle.builditbigger.BaseMainActivityFragment;
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 import com.udacity.gradle.builditbigger.R;
 
-public class MainActivityFragment extends Fragment
+import builditbigger.android.my.annin.jokeslibrary.JokesActivity;
+
+public class MainActivityFragment extends BaseMainActivityFragment
 {
     //Main Activity Fragment copied from main directory; AdView code removed
     public MainActivityFragment()
@@ -17,6 +23,9 @@ public class MainActivityFragment extends Fragment
     }
 
     TextView helloStringPaid;
+
+    private Button jokeButton;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,5 +35,26 @@ public class MainActivityFragment extends Fragment
 
         helloStringPaid = (TextView)root.findViewById(R.id.hello_paid_text_view);
 
+        jokeButton = (Button) root.findViewById(R.id.joke_button);
+
         return root;
-    }}
+    }
+
+    @Override
+    public void tellJoke()
+    {
+
+        EndpointsAsyncTask myTask = new EndpointsAsyncTask(this);
+        myTask.execute();
+    }
+
+    @Override
+    public void returnJokeData(String result)
+    {
+        Intent intent = new Intent(getActivity(), JokesActivity.class);
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(JokesActivity.JOKE_STRING, result);
+        startActivity(intent);
+    }
+
+    }
